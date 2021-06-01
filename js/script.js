136ff1f281861765a935cdb6c59b9c368da37676
lealdi <diegoleal1913@gmail.com>
2021-06-01 19:44:52 -0300

    Trabalhando alteracoes nas funcoes do arquivo JS

--- js/script.js
+++ js/script.js
@@ -13,4 +13,30 @@ let techs = [
     'node',
     'react'
 ]
+createCardsFromTechs(techs)
+function createCardsFromTechs(techs){
 
+    let cards = []
+
+    for(let tech of techs){ 
+        cards.push(createPairFromTech(tech))
+    }
+
+    return cards.flatMap(pair => pair)
+}
+
+function createPairFromTech(tech){
+    return {
+        id: createIdWidthTech(tech),
+        icon: tech,
+        flipped: false
+    },{
+        id: createIdWidthTech(tech),
+        icon: tech,
+        flipped: false
+    }
+}
+
+function createIdWidthTech(tech){
+    return tech + parseInt(Math.random() * 1000)
+}
