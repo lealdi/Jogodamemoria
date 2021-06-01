f6d6846b033da0357fa05aa2f64b4da14cb2056f
lealdi <diegoleal1913@gmail.com>
2021-06-01 18:40:03 -0300

    Iniciando arquivo JS

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const FRONT = "card_front"
+const BACK = "card_back"
+
+let techs = [
+    'bootstrap',
+    'css',
+    'electron',
+    'firebase',
+    'html',
+    'javascript',
+    'jquery',
+    'mongo',
+    'node',
+    'react'
+]
+
